82a47db3d554e998a98e0a43a5bbf87c7c09646d
Artem Kudria <ak98nneon@gmail.com>
2024-03-04 20:37:05 +0100

    MergeSort

---
+++ src/main/java/org/algo/sorting/MergeSort.java
@@ -0,0 +1,72 @@
+package org.algo.sorting;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        mergeSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void mergeSort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static void sort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            merge(arr, l, m, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[l + i];
+        }
+
+        for (int i = 0; i < n2; i++) {
+            R[i] = arr[m + 1 + i];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = l;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[] if any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+}
